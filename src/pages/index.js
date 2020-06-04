f296d38015ca61a318c9a4be271ee7c25139e379
Antonijo <a.djodj@gmail.com>
2020-06-04 13:30:18 +0200

    Starting lazy loading

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,24 +1,27 @@
-import React from "react"
+import React, { lazy, Suspense } from "react"
 import { Link } from "gatsby"
 import SEO from "../components/seo"
 import LandingHero from "../components/landing/LandingHero/LandingHero"
-import Intro from "../components/landing/Intro/Intro"
 import Features from "../components/landing/Features/Features"
 import Special from "../components/landing/Special/Special"
 import Stats from "../components/landing/Stats/Stats"
-import Doctors from '../components/landing/Doctors/Doctors'
+import Doctors from "../components/landing/Doctors/Doctors"
 import Layout from "../components/Layout"
 
+const Intro = lazy(() => import("../components/landing/Intro/Intro"))
+
 const IndexPage = () => (
+  <Suspense fallback={<div>Loading</div>}>
   <Layout>
     <SEO title="Home" />
-     <LandingHero />
-     <Intro />
-     <Features />
-     <Special />
-     <Stats />
-     <Doctors />
+    <LandingHero />
+    <Intro />
+    <Features />
+    <Special />
+    <Stats />
+    <Doctors />
   </Layout>
+  </Suspense>
 )
 
 export default IndexPage
